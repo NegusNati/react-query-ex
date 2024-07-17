856c1ff46dd42badb2634a11f861027389fca5cb
Negus <natnaelbirhanu22@gmail.com>
2024-07-17 21:06:08 +0300

    first time using useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //  const {} = useQuery({
+  //     queryKey: ['todos'],
+  //     queryFn: () => wait(1000).then(() => fetch('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX').then(res => res.json()))
+  //  })
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["thisKey"],
+    queryFn: () => wait(2000).then(() => "Hello"),
+  });
 
-  return (
-   <h1>Tan stack </h1>
-  )
+  if (isLoading) return <h2>it is loading</h2>;
+
+  if (error) {
+    return <h2>Something went wrong {error.message}</h2>;
+  }
+
+  return <h1>{data} </h1>;
 }
 
-function wait (duration) {
-  return new Promise(resolve => setTimeout(resolve, duration))
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
-export default App
+export default App;
