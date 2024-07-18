569ae26b85cbd1cd8f6163d02094b63036d0e2a0
Negus <natnaelbirhanu22@gmail.com>
2024-07-18 13:38:04 +0300

    update using useMutation

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ export function DisplayPosts() {
 }
 
 //useMutation ex=1 for post operation
-
+//Add post
 export const AddPost = () => {
   const {
     register,
@@ -66,7 +66,7 @@ export const AddPost = () => {
   } = useForm({
     defaultValues: {
       title: "",
-      body: "",
+      title: "",
     },
   });
   const postMutation = useMutation({
@@ -80,11 +80,11 @@ export const AddPost = () => {
       return response.data;
     },
   });
-
+  
   return (
     <div className=" ">
       <div>
-        <h1>useMutation example</h1>
+        <h1>useMutation example 1: Create</h1>
         <h2 className="text-2xl p-4">Posts </h2>
         <div className={" bg-stone-500 p-2 " + (postMutation.isIdle && " font-semibold ") + (postMutation.isSuccess&& " animate-pulse bg-green-400 ") + ( postMutation.isError && " animate-ping bg-red-400 ") + ( postMutation.isPending && " animate-bounce bg-yellow-400 ")} >
           {postMutation.isIdle && (
@@ -135,6 +135,84 @@ export const AddPost = () => {
 };
 
 
+//Update Post
+
+export const UpdatePost = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: "",
+      body: "",
+    },
+  });
+  const postMutation = useMutation({
+    mutationKey: ["updatedPost"],
+    mutationFn: async (updatedPost) => {
+      const response = await axios.put(
+        "https://jsonplaceholder.typicode.com/posts/1",
+        updatedPost
+      );
+      console.log(updatedPost);
+      return response.data;
+    },
+  });
+
+  return (
+    <div className=" ">
+      <div>
+        <h1>useMutation example 2 : Update</h1>
+        <h2 className="text-2xl p-4">Posts </h2>
+        <div className={" bg-stone-500 p-2 " + (postMutation.isIdle && " font-semibold ") + (postMutation.isSuccess&& " animate-pulse bg-green-400 ") + ( postMutation.isError && " animate-ping bg-red-400 ") + ( postMutation.isPending && " animate-bounce bg-yellow-400 ")} >
+          {postMutation.isIdle && (
+            <p>
+              Click the button to Update the post: it is in idle state until you add
+              a post
+            </p>
+          )}
+          {postMutation.isPending && <p>Updateing post...</p>}
+          {postMutation.isError && <p>Error Updateing post...</p>}
+          {postMutation.isSuccess && (
+            <p>Post updated successfully : {postMutation.data.title}</p>
+          )}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4 justify-start mt-8 pt-8 ">
+        <h2 className="text-2xl">Update a Post</h2>
+        <div className="  ">
+          <form
+            onSubmit={handleSubmit((data) => postMutation.mutate(data))}
+            className="flex flex-col gap-4"
+          >
+            <label htmlFor="title">Title</label>
+            <input
+              name="title"
+              {...register("title", { required: true })}
+              id="title"
+              placeholder="Enter title"
+              className="border-2 border-gray-300 p-2 rounded-md"
+            />
+            {errors.title && <p> {errors.title.message}</p>}
+            <label htmlFor="body">Body</label>
+            <textarea
+              name="body"
+              {...register("body", { required: true })}
+              id="body"
+              placeholder="Enter body"
+              className="border-2 border-gray-300 p-2 rounded-md"
+            />
+            {errors.body && <p> {errors.body.message}</p>}
+            <button type="submit">Update Post</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 
 
@@ -162,7 +240,8 @@ export const AddPost = () => {
 function App() {
   // return <TimeOutFetch />
   // return <DisplayPosts />;
-  return <AddPost />;
+  // return <AddPost />;
+  return <UpdatePost />;
 }
 
 export default App;
